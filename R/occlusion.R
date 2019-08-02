a11c009ce4d3b9c53a8b0f961332ba22b19deb0f
Saad Saif <msaadsaif5@gmail.com>
2019-08-02 19:22:47 +0200

    add occlusion class, add plot

--- R/occlusion.R
+++ R/occlusion.R
@@ -1,27 +1,39 @@
-#' @export
-getOcclusionPixelSet = function(occlusionImage){
+getOcclusionPixelSet <- function(occlusionImage){
   grays <- imager::grayscale(occlusionImage) #returns a cimg
   whiteRegion <- grays > 0 #returns a pixeset
-  boundingBox <- imager::bbox(whiteRegion) #pixelset of the bounding box
+  imager::bbox(whiteRegion) #pixelset of the bounding box
 }
 
-#' @export
-getImageRegion = function(img, pixelSet){
+getImageRegion <- function(img, pixelSet){
   region <- imager::crop.bbox(img, pixelSet)
 }
 
 #' @export
-plotOcclusionRegions = function(img, occImg, inpaintImg){
-  pixelSet <- getOcclusionPixelSet(occImg)
-  occlusionRegion <- getImageRegion(occImg, pixelSet)
-  beforeInpaintRegion <- getImageRegion(img, pixelSet)
-  afterInpaintRegion <- getImageRegion(inpaintImg, pixelSet)
+extractOcclusionRegions <- function(object){
+  if (!inherits(object, "inpaint"))
+    stop("Provided object is not an 'inpaint' class object")
   
+  # if (!object$imgIn | !object$imgInOcc | !object$imgOut)
+  #   stop("Input, occlusion input and inpaint output images are required")
+  
+  pixelSet <- getOcclusionPixelSet(object$imgInOcc)
+  
+  occRegion <- getImageRegion(object$imgInOcc, pixelSet)
+  imgInOccRegion <- getImageRegion(object$imgIn, pixelSet)
+  imgOutOccRegion <- getImageRegion(object$imgOut, pixelSet)
+  
+  occlusionObject <- list(occRegion = occRegion, imgInRegion = imgInOccRegion, imgOutRegion = imgOutOccRegion)
+  class(occlusionObject) <- "occlusion"
+  occlusionObject
+}
+
+#' @export
+plot.occlusion  <- function(x, ...){
   layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
   
-  plot(occlusionRegion, main = "Occlusion region")
-  plot(beforeInpaintRegion, main="Before inpainting")
-  plot(afterInpaintRegion, main="After inpainting")
+  plot(x$occRegion, main = "Occlusion region")
+  plot(x$imgInRegion, main="Before inpainting")
+  plot(x$imgOutRegion, main="After inpainting")
 }
 
 
